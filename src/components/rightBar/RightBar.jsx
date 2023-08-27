4980d4dbb9293d914375ddf6a11289fd831be7c3
unknown <prashant750379@gmail.com>
2023-08-27 22:27:23 +0530

    Initail Commit

---
+++ src/components/rightBar/RightBar.jsx
@@ -0,0 +1,112 @@
+import React from 'react'
+import "./rightBar.scss"
+export const RightBar = () => {
+  return (
+    <div className="rightBar">
+      <div className="container">
+        <div className="item">
+         <span> Suggestions for You</span> 
+         <div className="user">
+          <div className="userInfo">
+            <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <span> Prashant Gupta</span>
+          </div>
+          <div className="buttons">
+            <button>follow</button>
+            <button>dismiss</button>
+          </div>
+         </div>
+         
+         <div className="user">
+          <div className="userInfo">
+            <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <span> Prashant Gupta</span>
+          </div>
+          <div className="buttons">
+            <button>follow</button>
+            <button>dismiss</button>
+          </div>
+         </div>
+        </div>
+        <div className="item">
+          <span>Lastest Activites</span>
+          <div className="user">
+          <div className="userInfo">
+            <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <p>
+              <span> Prashant Gupta</span> change their cover picture
+            </p>
+          </div>
+              <span> 1 min ago</span>
+         </div>
+         <div className="user">
+          <div className="userInfo">
+            <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <p>
+              <span> Prashant Gupta</span> change their cover picture
+            </p>
+          </div>
+              <span> 1 min ago</span>
+         </div>
+         <div className="user">
+          <div className="userInfo">
+            <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <p>
+              <span> Prashant Gupta</span> change their cover picture
+            </p>
+          </div>
+              <span> 1 min ago</span>
+         </div>
+         <div className="user">
+          <div className="userInfo">
+            <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <p>
+              <span> Prashant Gupta</span> change their cover picture
+            </p>
+          </div>
+              <span> 1 min ago</span>
+         </div>
+        </div>
+        <div className="item">
+          <span>Online Friends</span>
+          <div className="user">
+          <div className="userInfo">
+            <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <div className="online" />  
+              <span> Prashant Gupta</span>
+          </div>
+         </div>
+         <div className="user">
+          <div className="userInfo">
+            <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <div className="online" />  
+              <span> Prashant Gupta</span>
+          </div>
+         </div>
+         <div className="user">
+          <div className="userInfo">
+            <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <div className="online" />  
+              <span> Prashant Gupta</span>
+          </div>
+         </div>
+         <div className="user">
+          <div className="userInfo">
+            <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <div className="online" />  
+              <span> Prashant Gupta</span>
+          </div>
+         </div>
+         <div className="user">
+          <div className="userInfo">
+            <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <div className="online" />  
+              <span> Prashant Gupta</span>
+          </div>
+         </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default RightBar;
